14c2b9b5bc0c0ed3f4dc00e3ab3ef45cc7dd1676
Tri Nguyen <zymethyang@gmail.com>
2018-04-27 08:36:41 +0700

    debug

--- src/studentRouter.js
+++ src/studentRouter.js
@@ -60,7 +60,6 @@ studentRouter.route('/add/:id')
         next();
     })
     .get((req, res, next) => {
-
         Student.findOne({ id: req.params.id })
             .then(student => {
                 var time = new Date().getTime();
@@ -111,6 +110,66 @@ studentRouter.route('/add/:id')
             res.json(false);
             console.log(error);
         });
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /event');
+    })
+    .delete((req, res, next) => {
+        res.end('DELETE operation not supported on /event');
+    });
+
+
+studentRouter.route('/scan')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.header('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .get((req, res, next) => {
+        res.statusCode = 403;
+        res.end('GET operation not supported on /event');
+    })
+    .post((req, res, next) => {
+        console.log(req.body);
+        /*
+        Student.findOne({ id: req.params.id })
+            .then(student => {
+                var time = new Date().getTime();
+                var client = mqtt.connect({
+                    host: 'm13.cloudmqtt.com',
+                    port: '14250',
+                    password: 'B9Io8J5H88fP',
+                    username: 'jepjknnb'
+                })
+
+                client.on('connect', function () {
+                    console.log('Connected');
+                    client.subscribe('student');
+                    client.publish('student', JSON.stringify(student));
+                    client.end();
+                });
+                Events.findOne({ $and: [{ tgbd: { $lte: Math.floor(time / 1000) } }, { tgkt: { $gte: Math.floor(time / 1000) } }] }).then(events => {
+                    if (events) {
+                        let tmpStudent = student;
+                        tmpStudent.sktg.push(events.ten);
+                        tmpStudent.diem = tmpStudent.diem + events.diemCong;
+                        Student.updateOne({ id: req.params.id }, tmpStudent).then(std => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(true);
+
+                        })
+                    }
+                })
+            })
+            .catch(err => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json([]);
+            });
+            */
     })
     .put((req, res, next) => {
         res.statusCode = 403;
